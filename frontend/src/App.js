5bd4aeefa2d0e8f9a7c8cffc39cfe8c2745f55cd
f1x3d49 <davidnicoara28@gmail.com>
2023-06-02 14:59:48 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-6xl text-lred">Hello</div>;
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lred: " hsl(356, 100%, 66%)",
+        vlred: "hsl(355, 100%, 74%)",
+        vdblue: "hsl(208, 49%, 24%)",
+        white: "hsl(0, 0%, 100%)",
+        gblue: "hsl(240, 2%, 79%)",
+        vdgblue: "hsl(207, 13%, 34%)",
+        vdbblue: "hsl(240, 10%, 16%)",
+      },
+
+      fontFamily: {
+        overpass: ["Overpass", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
