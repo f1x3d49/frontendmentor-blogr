c84930172fa80985454d85910676a7c3b30e6e86
f1x3d49 <davidnicoara28@gmail.com>
2023-06-03 19:18:25 +0300

    working

--- frontend/src/pages/Intro.js
+++ frontend/src/pages/Intro.js
@@ -1,15 +1,20 @@
 import React from "react";
-import introd from "../images/bg-pattern-intro-desktop.svg";
-import introm from "../images/bg-pattern-intro-mobile.svg";
+import Navbar from "../components/Navbar";
 
 const Intro = () => {
   return (
-    <section className="w-auto h-96 bg-gradient-to-r from-[#ff8f70] to-[#ff3d54] rounded-bl-[5rem] overflow-hidden">
-      <picture>
-        <source media="(min-width: 768px)" srcset={introd} />
-        <source srcset={introm} />
-        <img src={introm} alt="" className="md:mt-[-550px] scale-125" />
-      </picture>
+    <section className="flex flex-col justify-start  items-center w-auto h-96 bg-gradient-to-r from-[#ff8f70] to-[#ff3d54] rounded-bl-[5rem] overflow-hidden">
+      <Navbar />
+      <div className="flex flex-col items-center my-auto">
+        <h1 className="text-5xl text-white font-overpass font-semibold">
+          A modern publishing platform
+        </h1>
+        <p>Grow your audience and build your online brand</p>
+        <div className="flex gap-6">
+          <button>Start for free</button>
+          <button>Learn More</button>
+        </div>
+      </div>
     </section>
   );
 };
