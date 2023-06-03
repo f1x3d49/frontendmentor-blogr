d08487788077b319595d56dcef99110905855c01
f1x3d49 <davidnicoara28@gmail.com>
2023-06-03 23:21:48 +0300

    workign on the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex items-center justify-between md:py-12 md:px-36">
+      <div className="flex items-center gap-8">
+        <Logo className="scale-75" />
+        <ul className="flex">
+          <li className="p-4">Product</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Connect</li>
+        </ul>
+      </div>
+      <div className="flex gap-12">
+        <button className="text-white font-ubuntu font-medium">Login</button>
+        <button className="rounded-full hover:bg-vlred bg-white shadow-sm font-ubuntu font-medium text-vlred hover:text-white min-w-auto px-8 py-2">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/Intro.js
+++ frontend/src/pages/Intro.js
@@ -3,16 +3,22 @@ import Navbar from "../components/Navbar";
 
 const Intro = () => {
   return (
-    <section className="flex flex-col justify-start  items-center w-auto h-96 bg-gradient-to-r from-[#ff8f70] to-[#ff3d54] rounded-bl-[5rem] overflow-hidden">
+    <section className="flex flex-col justify-start  items-center w-auto h-96 bg-gradient-to-r from-[#ff8f70] to-[#ff3d54] rounded-bl-[5rem] ring-2 ring-vlred">
       <Navbar />
-      <div className="flex flex-col items-center my-auto">
-        <h1 className="text-5xl text-white font-overpass font-semibold">
+      <div className="flex flex-col items-center my-auto px-12 md:px-0 text-center">
+        <h1 className="md:text-5xl text-3xl text-center text-white font-overpass font-semibold">
           A modern publishing platform
         </h1>
-        <p>Grow your audience and build your online brand</p>
-        <div className="flex gap-6">
-          <button>Start for free</button>
-          <button>Learn More</button>
+        <p className="text-[16px] text-white font-overpass py-8 ">
+          Grow your audience and build your online brand
+        </p>
+        <div className="flex gap-4">
+          <button className="rounded-full bg-white hover:bg-vlred hover:text-white shadow-sm min-w-auto p-3 text-center text-lred font-ubuntu font-bold">
+            Start for Free
+          </button>
+          <button className="rounded-full min-w-auto text-white p-3 border-2 hover:bg-white hover:text-lred border-white text-center font-ubuntu font-bold">
+            Learn More
+          </button>
         </div>
       </div>
     </section>
