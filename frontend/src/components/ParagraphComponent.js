0894e721117b5d73a2328fdeb2782205bab719a3
f1x3d49 <davidnicoara28@gmail.com>
2023-06-09 19:49:17 +0300

    working

--- frontend/src/components/ParagraphComponent.js
+++ frontend/src/components/ParagraphComponent.js
@@ -2,14 +2,11 @@ import React from "react";
 
 const ParagraphComponent = ({ title, content }) => {
   return (
-    <div
-      className="flex flex-col md:text-left justify-start
-    "
-    >
-      <h1 className="text-xl font-overpass font-semibold text-vdblue px-20 pb-4">
+    <div className="md:text-left md:pl-20 md:pr-56">
+      <h1 className="text-xl font-overpass font-semibold text-vdblue  pb-4">
         {title}
       </h1>
-      <p className="text-sm font-overpass text-vdgblue leading-relaxed px-1 text-center">
+      <p className="text-sm font-overpass text-vdgblue leading-relaxed  text-center">
         {content}
       </p>
     </div>
--- frontend/src/pages/PresentationSection.js
+++ frontend/src/pages/PresentationSection.js
@@ -1,5 +1,6 @@
 import React from "react";
-import editor from "../images/illustration-editor-mobile.svg";
+import editorm from "../images/illustration-editor-mobile.svg";
+import editord from "../images/illustration-editor-desktop.svg";
 import ParagraphComponent from "../components/ParagraphComponent";
 
 const PresentationSection = () => {
@@ -8,9 +9,13 @@ const PresentationSection = () => {
       <h1 className="text-xl md:text-2xl font-overpass text-vdblue font-semibold ">
         Designed for the future
       </h1>
-      <div className="flex flex-col md:flex-row gap-8 ">
-        <img src={editor} alt="editor" className="md:order-last" />
-        <div className="flex flex-col gap-8 md:w-1/2">
+      <div className="flex flex-col md:flex-row gap-8">
+        <picture className="md:order-last md:absolute md:scale-90 md:left-[50%] md:top-[60%]">
+          <source media="(min-width: 400px)" srcSet={editord} />
+          <source media="(min-width: 280px)" srcSet={editorm} />
+          <img src={editorm} alt="editor" />
+        </picture>
+        <div className="flex flex-col  gap-8 md:w-1/2 md:mt-20">
           <ParagraphComponent
             title="Introducing an extensible editor"
             content="Blogr features an exceedingly intuitive interface wich lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog."
