c263f5c225a10f1c4831d054a20cbf896780c7f8
f1x3d49 <davidnicoara28@gmail.com>
2023-06-10 22:00:10 +0300

    working on the semi last section

--- frontend/src/pages/PhoneSection.js
+++ frontend/src/pages/PhoneSection.js
@@ -4,22 +4,21 @@ import circles from "../images/bg-pattern-circles.svg";
 
 const PhoneSection = () => {
   return (
-    <section className="flex w-full bg-gradient-to-r from-[#2c2d3f] to-[#3f4164] md:h-[400px] md:mt-36 overflow-hidden rounded-tr-[6rem] rounded-bl-[6rem]">
-      <div>
-        <img
-          src={circles}
-          alt="circles"
-          className="relative z-0 origin-top md:ml-64 rotate-90"
-        />
-        <img
-          src={phones}
-          alt="phones"
-          className="absolute md:top-[1125px] md:left-24"
-        />
-      </div>
-      <div className="h-full aboslute text-left font-overpass font-semibold flex flex-col justify-center gap-6">
+    <section className=" w-full bg-gradient-to-r from-[#2c2d3f] to-[#3f4164] md:h-[400px] md:mt-36 mt-64 overflow-hidden rounded-tr-[6rem] rounded-bl-[6rem]">
+      <img
+        src={circles}
+        alt="circles"
+        className="relative z-0 origin-top ml-72 mt-16 md:mt-0 mb-32 md:mb-0 scale-150 md:scale-100 rotate-90 md:ml-64"
+      />
+      <img
+        src={phones}
+        alt="phones"
+        className="absolute md:top-[1125px] top-[1375px] md:left-24"
+      />
+
+      <div className="h-full absolute top-[160%] md:top-[140%] md:left-[45%] md:pr-80 md:text-left text-center px-8 md:px-0 font-overpass font-semibold flex flex-col justify-center gap-6">
         <h1 className="text-white text-4xl">State of the Art Infrastructure</h1>
-        <p>
+        <p className="text-gblue font-light leading-relaxed">
           With reliability and speed in mind, worldwide data centers provide the
           backbone for ultra-fast conectivity. This ensures your site will load
           instantly, no matter where your readers are, keeping your site
